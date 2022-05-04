94e47c11ba80309a41ba25b69ea4338d1f109577
Rodrigo Isaac Avendaño Ortíz <99101837+RodrigoAvendano@users.noreply.github.com>
2022-05-03 21:46:20 -0500

    Añadido linter, método getEmail y su prueba de unidad

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,11 +1,28 @@
-const Reader = require("../../lib/utils/reader")
-const StudentService = require("../../lib/services/StudentService")
+const Reader = require("../../lib/utils/reader");
+const StudentService = require("../../lib/services/StudentService");
 
-describe("Test for StudentService class",()=>{
-    test("1. Testing getAllStudents method",()=>{
-        const studentList = Reader.readJsonFile("visualpartners.json")
-        const partnerList = StudentService.getAllStudents(studentList)
-        const totalPartners = partnerList.length
-        expect(totalPartners ).toBe(51)
-    })
-})
+describe("Test for StudentService class", () => {
+    test("1. Testing getAllStudents method", () => {
+        const studentList = Reader.readJsonFile("visualpartners.json");
+        const partnerList = StudentService.getAllStudents(studentList);
+        const totalPartners = partnerList.length;
+        expect(totalPartners).toBe(51);
+    });
+
+    test("2. Testing getEmail method", () => {
+        const studentList = Reader.readJsonFile("visualpartners.json");
+        const mailList = StudentService.getEmail(studentList);
+        const expected = [
+            {
+                id: "6264d5d8878a117a9c57c5c4",
+                name: "Claudia",
+                email: "Howell@visualpartnership.xyz",
+                credits: 227,
+                enrollments: ["Visual Thinking Avanzado"],
+                previousCourses: 5,
+                haveCertification: true,
+            },
+        ];
+        expect(mailList).toEqual(expect.arrayContaining(expected));
+    });
+});
